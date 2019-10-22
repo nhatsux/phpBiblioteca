fdb91e38ff924232eb4f1f0c33f1d0b747f2d94f
Dihang117 <49285640+Dihang117@users.noreply.github.com>
2019-10-21 19:49:25 -0500

    Update DataAccess.php

--- Biblioteca/model/DataAccess.php
+++ Biblioteca/model/DataAccess.php
@@ -27,6 +27,60 @@ class DataAccess{
         }
         return $Ret;
     }
+
+    function ejecutarConsulta($psConsulta){
+		$arrRS = null;
+		$rst = null;
+		$arrLin = null;
+		$sValCol = "";
+		$i=0;
+		$j=0;
+			if ($psConsulta == ""){
+		       throw new Exception("DataAccess->ejecutarConsulta: falta indicar la consulta");
+			}
+			if ($this->Conexion == -1){
+				throw new Exception("DataAccess->ejecutarConsulta: falta conectar la base");
+			}
+			try{
+				$rst = pg_query($this->Conexion, $psConsulta);
+			}catch(Exception $e){
+				throw $e;
+			}
+			if ($rst){
+				while($arrLin = pg_fetch_row($rst)){ 
+					foreach($arrLin as $sValCol){
+						$arrRS[$i][$j] = $sValCol;
+						$j++;
+					}
+					$j=0;
+					$i++;
+				}
+			}
+			else{
+				throw new Exception($this->Conexion->error);
+			}
+			return $arrRS;
+		}
+		
+		/*Ejecuta en la base de datos el comando que recibió por parámetro
+		Regresa el número de registros afectados por el comando*/
+      	function ejecutarComando($psComando){
+		$nAfectados = -1;
+	       if ($psComando == ""){
+		       throw new Exception("DataAccess->ejecutarComando: falta indicar el comando");
+			}
+			if ($this->Conexion == -1){
+				throw new Exception("DataAccess->ejecutarComando: falta conectar la base");
+			}
+			try{
+	       	   $bResult=pg_query($this->Conexion, $psComando);
+			   $nAfectados = pg_affected_rows($bResult);
+			}catch(Exception $e){
+				throw $e;
+			}
+			return $nAfectados;
+		}
+	}
 }
 
 ?>
